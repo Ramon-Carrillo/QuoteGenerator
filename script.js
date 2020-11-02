4d3c18e5f913e5ffa98102cd988222215dbf410b
Ramon-Carrillo <carrillojrramon>
2020-11-02 11:41:35 -0600

    changed the proxy

--- script.js
+++ script.js
@@ -21,7 +21,7 @@ function removeLoadingSpinner() {
 // Get Quote From API
 async function getQuote() {
   showLoadingSpinner();
-  const proxyUrl = 'https://evening-falls-28731.herokuapp.com/';
+  const proxyUrl = 'https://cors-anywhere.herokuapp.com/';
   const apiUrl = 'http://api.forismatic.com/api/1.0/?method=getQuote&lang=en&format=json';
   try {
       const response = await fetch(proxyUrl + apiUrl);
