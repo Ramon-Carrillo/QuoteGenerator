c9270c42daab3a9afdcd9da63e960355c0b6479c
Ramon-Carrillo <carrillojrramon>
2020-11-02 11:46:42 -0600

    adjusted js file

--- script.js
+++ script.js
@@ -1,61 +1,56 @@
-const quoteContainer = document.getElementById('quote-container');
-const quoteText = document.getElementById('quote');
-const authorText = document.getElementById('author');
-const twitterBtn = document.getElementById('twitter');
-const newQuoteBtn = document.getElementById('new-quote');
-const loader = document.getElementById('loader');
+const quoteContainer = document.getElementById("quote-container");
+const quoteText = document.getElementById("quote");
+const authorText = document.getElementById("author");
+const twitterBtn = document.getElementById("twitter");
+const newQuote = document.getElementById("new-quote");
+const loader = document.getElementById("loader");
 
 function showLoadingSpinner() {
   loader.hidden = false;
   quoteContainer.hidden = true;
 }
 
-
 function removeLoadingSpinner() {
-  if (!loader.hidden){
+  if (!loader.hidden) {
     quoteContainer.hidden = false;
     loader.hidden = true;
   }
 }
 
-// Get Quote From API
+// Fetch quote from forrismatic
 async function getQuote() {
   showLoadingSpinner();
-  const proxyUrl = 'https://cors-anywhere.herokuapp.com/';
-  const apiUrl = 'http://api.forismatic.com/api/1.0/?method=getQuote&lang=en&format=json';
+  const proxyUrl = `https://cors-anywhere.herokuapp.com/`;
+  const apiUrl = `http://api.forismatic.com/api/1.0/?method=getQuote&lang=en&format=json`;
   try {
-      const response = await fetch(proxyUrl + apiUrl);
-      const data = await response.json();
-      // If Author is blank, add 'Unknown'
-      if (data.quoteAuthor === '') {
-        authorText.innerText = 'Unknown'
-      }else {
-        authorText.innerText = data.quoteAuthor;
-      }
-      // Reduce font size for long quotes
-      if (data.quoteText.length > 120) {
-        quoteText.classList.add('long-quote');
-      }else {
-        quoteText.classList.remove('long-quote');
-      }
-      quoteText.innerText = data.quoteText
-      // Stop Loader, Show Quote
-      removeLoadingSpinner();
+    const response = await fetch(proxyUrl + apiUrl);
+    const data = await response.json();
+    data.quoteAuthor !== ""
+      ? (authorText.innerText = data.quoteAuthor)
+      : (authorText.innerText = data.quoteAuthor);
+
+    // Reduce font-size for long quotes
+    data.quoteText.length > 120
+      ? quoteText.classList.add("long-quote")
+      : quoteText.classList.remove("long-quote");
+
+    quoteText.innerText = data.quoteText;
+    removeLoadingSpinner();
   } catch (error) {
-      getQuote();
+    console.log("Whoops could not retrieve quotes: " + error);
   }
 }
 
+// Twitter functionality for tweeting quote
 function tweetQuote() {
   const quote = quoteText.innerText;
   const author = authorText.innerText;
   const twitterUrl = `https://twitter.com/intent/tweet?text=${quote} - ${author}`;
-  window.open(twitterUrl, '_blank');
+  window.open(twitterUrl, "_blank");
 }
 
-// Event Listeners
-newQuoteBtn.addEventListener('click', getQuote);
-twitterBtn.addEventListener('click', tweetQuote);
+// EventListeners
+newQuote.addEventListener("click", getQuote);
+twitterBtn.addEventListener("click", tweetQuote);
 
-// On Load
 getQuote();
